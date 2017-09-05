5610f8c4fa0e25657100a40a6218f51980f7dce6
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-09-05 08:23:51 +0200

    Oprettet Client, og klaret Assignment C

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,18 @@ namespace Client
 {
     class Client
     {
+        public void Start()
+        {
+            using(TcpClient client = new TcpClient("localhost", 7))
+            using (NetworkStream ns = client.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                string line = Console.ReadLine();
+                sw.WriteLine(line);
+                sw.Flush();
+                string myLine = sr.ReadLine();
+            }
+        }
     }
 }
