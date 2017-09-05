3798b4aa34cade738352405d0576cb0e9174f76f
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-09-05 08:17:39 +0200

    Oprettet Server samt class, og lavet TcpClients osv. også lavet Strings og lingnende Første opgave er "kinda" lavet, må ikke skrive i Console.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MiniChatServer
+namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Server server = new Server();
-            server.Start();
-
-            Console.ReadLine();
         }
     }
 }
